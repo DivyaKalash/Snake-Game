5c403c98b2d1752436dd4d76697fcc3d110442c0
DivyaKalash <67993272+DivyaKalash@users.noreply.github.com>
2020-12-26 23:37:28 +0530

    snake game completed added food and scoreboard

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -12,16 +12,22 @@ class Snake:
 
     def __init__(self):
         self.segments = []
-        self.createSnake()
+        self.createsnake()
         self.head = self.segments[0]
 
-    def createSnake(self):
+    def createsnake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
